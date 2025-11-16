53e08c7f97a96236d0ba55630a8817c353aaf5e5
Bhoomi Deshpande <01fe23bcs145@kletech.ac.in>
2025-11-16 06:18:22 +0530

    Add links to flight numbers in Home component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,7 @@
+// frontend/src/pages/Home.jsx
 import React, { useEffect, useState } from 'react'
 import api from '../services/api'
+import { Link } from 'react-router-dom'
 
 export default function Home(){
   const [flights, setFlights] = useState([])
@@ -10,7 +12,9 @@ export default function Home(){
       {flights.length===0 ? <p>No flights yet</p> : (
         <ul>
           {flights.map(f=> (
-            <li key={f._id}>{f.flightNumber} - {f.origin} ➜ {f.destination} | ${f.price}</li>
+            <li key={f._id}>
+              <Link to={`/flights/${f._id}`}>{f.flightNumber}</Link> - {f.origin} ➜ {f.destination} | ₹{f.price}
+            </li>
           ))}
         </ul>
       )}
